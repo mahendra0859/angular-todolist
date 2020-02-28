3d02b015eee0e84bdc819bb330e2f9d1d7bb87f7
Mahendra <mahendra0859@gmail.com>
2020-02-28 17:49:29 +0530

    Basic todolist added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { TodoComponent } from "./components/todo/todo.component";
+import { AboutComponent } from "./components/pages/about/about.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: TodoComponent
+  },
+  { path: "about", component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
